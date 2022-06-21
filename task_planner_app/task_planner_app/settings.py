90b6dfa1e0d5504215f4f4c57a0035b04f5dceb1
Nguyen Hoang <nguyenh3029@gmail.com>
2022-06-21 17:01:01 +1000

    started homepage

--- task_planner_app/task_planner_app/settings.py
+++ task_planner_app/task_planner_app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 import task_planners
@@ -166,6 +167,11 @@ DEFAULT_MAX_NUM = 200
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA = '/images/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
