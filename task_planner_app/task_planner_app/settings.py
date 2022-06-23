4bf47b79f010c4f9d3bf9544e0940793139ab38c
notnewton2803 <76431620+notnewton2803@users.noreply.github.com>
2022-06-23 21:42:40 +1000

    Commit Password Reset Template
    Commiting the html templates and urls.

--- task_planner_app/task_planner_app/settings.py
+++ task_planner_app/task_planner_app/settings.py
@@ -176,3 +176,12 @@ MEDIA = '/images/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True 
+EMAIL_HOST_USER = 'newtonphan112233@gmail.com'
+EMAIL_HOST_PASSWORD = '!Owchesnobs098765'
