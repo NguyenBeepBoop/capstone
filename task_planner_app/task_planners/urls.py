bd1d20438bb130b19c4b80c53f0116508b6bb113
notnewton2803 <76431620+notnewton2803@users.noreply.github.com>
2022-06-23 21:53:43 +1000

    Update urls.py

--- task_planner_app/task_planners/urls.py
+++ task_planner_app/task_planners/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('recovery', views.recovery, name="recovery"),
+    path('register/', register, name="register"),
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="password_reset.html"), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_sent.html"), name="password_reset_done"),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form"), name="password_reset_confirm"),
