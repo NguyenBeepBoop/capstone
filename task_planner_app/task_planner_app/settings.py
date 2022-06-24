a07b747206a9569d786837497de622b3694f7a43
notnewton2803 <76431620+notnewton2803@users.noreply.github.com>
2022-06-24 20:52:22 +1000

    Email verification
    Now able to send an email for verification.

--- task_planner_app/task_planner_app/settings.py
+++ task_planner_app/task_planner_app/settings.py
@@ -192,8 +192,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #SMTP Configuration
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
+EMAIL_HOST_USER = 'peterisachicken123@gmail.com'
+EMAIL_HOST_PASSWORD = 'bimjdvlydmofcxwk'
 EMAIL_USE_TLS = True 
-EMAIL_HOST_USER = 'newtonphan112233@gmail.com'
-EMAIL_HOST_PASSWORD = '!Owchesnobs098765'
+
+
+
--- task_planner_app/task_planners/urls.py
+++ task_planner_app/task_planners/urls.py
@@ -1,3 +1,5 @@
+from atexit import register
+from users.views import register
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
@@ -7,6 +9,6 @@ urlpatterns = [
     path('register/', register, name="register"),
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="password_reset.html"), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_sent.html"), name="password_reset_done"),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form"), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done"), name="password_reset_complete"),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done.html"), name="password_reset_complete"),
 ]
