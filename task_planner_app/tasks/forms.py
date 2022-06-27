487bb433f0cf84b299cc4ce6901a5f960969d1d5
joshuakitara <joshuakitara9029@gmail.com>
2022-06-27 20:45:07 +1000

    push connecting task groups, lists and cards

--- task_planner_app/tasks/forms.py
+++ task_planner_app/tasks/forms.py
@@ -1,13 +1,22 @@
 from django import forms
 from datetime import datetime
 from django.utils import timezone
-from .models import Task
+from .models import Task, TaskList
 
 class TaskForm(forms.ModelForm):
     
     class Meta:
         model = Task
-        fields = ['task_name', 'task_description', 'deadline', 'status', 'priority']
+        fields = ['name', 'description', 'deadline', 'status', 'priority', 'task_list', 'assignee']
+        widgets = {
+            'deadline': forms.DateInput(attrs={'type':'datetime-local'})
+        }
+
+class TaskListForm(forms.ModelForm):
+
+    class Meta:
+        model = TaskList
+        fields = '__all__'
         widgets = {
             'deadline': forms.DateInput(attrs={'type':'datetime-local'})
         }
--- task_planner_app/tasks/views.py
+++ task_planner_app/tasks/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from .models import Task, TaskList
-from .forms import TaskForm
+from .models import Task, TaskList, TaskGroup
+from .forms import TaskForm, TaskListForm
 from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
-
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 
 # Create your views here.
 class TaskCreateView(CreateView):
@@ -20,8 +21,8 @@ class TaskCreateView(CreateView):
 
 class TaskListCreateView(CreateView):
     model = TaskList
-    fields = ['list_name']
-    template_name = "lists_template.html"
+    form_class = TaskListForm
+    template_name = "task_list_create.html"
     success_url = reverse_lazy("tasks:lists")
 
     def get_context_data(self, **kwargs):
@@ -30,27 +31,52 @@ class TaskListCreateView(CreateView):
         context['task_lists'] = TaskList.objects.all()
         return context
 
-class TaskListDisplView(CreateView):
-    model = TaskList
-    fields = ['list_name']
-    template_name = "task_list.html"
-    success_url = reverse_lazy("tasks:lists")
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tasks'] = self.get_object().task_set.all()
-        return context
+@login_required
+def TaskListDisplView(request, pk):
+    template = "task_list.html"
+    tasklists = TaskGroup.objects.get(pk=pk).tasklist_set.all()
+    context = {
+        "tasklists": tasklists
+    }
+    return render(request, template, context)
+    
+@login_required
+def TaskDisplView(request, pk):
+    template = "task_list.html"
+    tasklists = TaskList.objects.get(pk=pk).task_set.all()
+    context = {
+        "tasklists": tasklists
+    }
+    return render(request, template, context)
 
 class TaskGroupCreateView(CreateView):
-    model = TaskList
-    fields = ['list_name']
-    template_name = "lists_template.html"
-    success_url = reverse_lazy("tasks:lists")
-
+    model = TaskGroup
+    fields = '__all__'
+    template_name = 'task_group_create.html'    
+    success_url = reverse_lazy("tasks:groups")
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         #context['tasks'] = self.get_object().task_set.all()
-        context['task_lists'] = TaskList.objects.all()
+        context['task_groups'] = TaskGroup.objects.all()
         return context
 
+    def form_valid(self, form):
+        if super().form_valid(form):
+            curr = form.save(commit=False)
+            curr.owner = self.request.user
+            curr.save()
+        return redirect(self.success_url)
+
+
 # Create your views here.
+class TaskDetailView(DetailView):
+    model = Task
+    fields = ['task_name', 'task_description', 'deadline', 'status', 'assignee', 'priority']
+    template_name = "task_details.html"
+    
+    def get_context_data(self, **kwargs):
+        context= super().get_context_data(**kwargs)
+        context['tasks'] = Task.objects.all()
+        return context
+
