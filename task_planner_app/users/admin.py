96ccc374bcea4cbc96fd685e9955e7690f90ffa8
notnewton2803 <76431620+notnewton2803@users.noreply.github.com>
2022-07-18 20:28:34 +1000

    update

--- task_planner_app/users/admin.py
+++ task_planner_app/users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from users.models import User
+from users.models import User, FriendList, FriendRequest
 # Register your models here.
 
 class UserAdmin(UserAdmin):
@@ -14,3 +14,26 @@ class UserAdmin(UserAdmin):
     
 admin.site.register(User, UserAdmin)
 
+class FriendListAdmin(admin.ModelAdmin):
+    list_filter = ['user']
+    list_display = ['user']
+    search_fields = ['user']
+    readonly_fields = ['user',]
+
+    class Meta:
+        model = FriendList
+
+
+admin.site.register(FriendList, FriendListAdmin)
+
+
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_filter = ['sender', 'receiver']
+    list_display = ['sender', 'receiver',]
+    search_fields = ['sender__username', 'receiver__username']
+
+    class Meta:
+        model = FriendRequest
+
+
+admin.site.register(FriendRequest, FriendRequestAdmin)
